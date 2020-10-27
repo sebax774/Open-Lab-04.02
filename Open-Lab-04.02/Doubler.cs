900b37fb3e6c18fb934bb6cfe95b18453e739fb2
sebax774 <bugansebastian@gmail.com>
2020-10-27 14:06:52 +0100

    vypracovanie

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string a = "";
+            for (int i = 0; i < original.Length; i++)
+            {
+                a = a + original[i] + original[i];
+            }
+            return a;
         }
     }
 }
